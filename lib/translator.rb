d29c1f1dc8a5b1f3790053d752e493164244c63e
Matthew Heavner <mrh9ud@virginia.edu>
2019-10-25 02:28:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,15 +13,17 @@ def load_library(emoticon_file)
   emoticon_hash
 end
 
-def get_japanese_emoticon(emoticon_file, emoticon)
+def get_japanese_emoticon(emoticon_file, emoticon_key)
   emoticon_hash = load_library(emoticon_file)
-  jap_emoticon = emoticon_hash['get_emoticon'][emoticon]
+  jap_emoticon = emoticon_hash['get_emoticon'][emoticon_key]
   jap_emoticon ? jap_emoticon : 'Sorry, that emoticon was not found'
 end
 
 
-def get_english_meaning(emoticon_file, emoticon)
+def get_english_meaning(emoticon_file, emoticon_key)
   emoticon_hash = load_library(emoticon_file)
-  eng_meaning = emoticon_hash['get_meaning'][emoticon]
+  eng_meaning = emoticon_hash['get_meaning'][emoticon_key]
   eng_meaning ? eng_meaning : 'Sorry, that emoticon was not found'
 end
+
+get_english_meaning("./lib/emoticons.yml", "(Ｔ▽Ｔ)")
